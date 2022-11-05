1a1ea5bd2666f4ddf11847a35f84644b111555a0
bitzzz <104964037+bitzok@users.noreply.github.com>
2022-11-04 20:12:58 -0500

    Delete literarios.py

--- literarios.py
+++
@@ -1,5 +0,0 @@
-class Literarios(Libro):
-    def __init__(self, codigo, nombre, autor, stock, edicion, genero, editorial):
-        super().__init__(codigo, nombre, autor, stock, edicion)
-        self.genero = genero
-        self.editorial = editorial
