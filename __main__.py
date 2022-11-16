9d2f0468764578c2ac790d74269f7e37fc8d9685
DanielTriveno <daniel.triveno@unmsm.edu.pe>
2022-11-16 00:52:14 -0500

    Clases de tkinter eliminadas, Login por consola

--- __main__.py
+++ __main__.py
@@ -1,68 +1,37 @@
-from tkinter import *
-from tkinter.messagebox import *
-from Funciones_DB import *
-
 import sqlite3
-
-import Clase_Admin as Lib
-import Clase_Clientes as Cli
 
 db_nombre = "login.db"
 
-def root():
-    global screen_login
-    global username
-    global password
-    screen_login = Tk()
-    screen_login.title("INGRESAR USUARIO")
-    screen_login.geometry("350x150+500+250")
-
-    Label(screen_login, text = "Usuario").pack()
-    username = Entry(screen_login, font="Ubuntu 12", justify="center")
-    username.pack()
-
-    Label(screen_login, text = "Contraseña").pack()
-    password = Entry(screen_login, show = "*", font="Ubuntu 12", justify="center")
-    password.pack()
-
-    enter = Button(text ="Login", font = "Ubuntu 14", command = login)
-    enter.config(activebackground = "dark blue")
-    enter.pack()
-
-    screen_login.mainloop()
 
 def login():
-    #connection to database
-    db=sqlite3.connect('login.db')
+    # connection to database
+    db = sqlite3.connect('login.db')
     c = db.cursor()
+    print("     LOGIN BIBLIOTECA")
+    user = input("Ingrese usuario: ")
+    passw = input("Ingrese contraseña: ")
 
-    user = username.get()
-    passw = password.get()
-    
     c.execute('SELECT * FROM users WHERE user = ? AND password = ?', (user, passw))
 
     if c.fetchall():
-        c.execute('SELECT lvl FROM users WHERE user = ? AND password = ?', (user, passw))
+        c.execute(
+            'SELECT lvl FROM users WHERE user = ? AND password = ?', (user, passw))
         lvl = c.fetchall()
 
-        showinfo(title = "Usuario correcto!", message = "Ingresando al sistema")
-        screen_login.destroy()
-
-
+        print("Usuario correcto!")
+        print("Ingresando al sistema")
         if lvl == [(1,)]:
-            app = Cli.Pedidos()
-            app1 = Cli.Pedidos.ventanaPrincipal(app)
+            print("Usuario")
         elif lvl == [(3,)]:
-            app = Lib.Admin()
-            app1 = Lib.Admin.ventanaPrincipal(app)
-        else: 
+            print("Admin")
+        else:
             print("Algo salió mal")
             print(lvl)
-        
+
     else:
-        showerror(title = "Ups", message = "El usuario o contraseña está incorrecto")
+        print("Ups, El usuario o contraseña está incorrecto")
 
     c.close()
 
 if __name__ == "__main__":
-    root()
+    login()
