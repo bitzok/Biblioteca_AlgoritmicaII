abe3ca01cb0a92541f171ad6bc1498e12f61514e
Dwar325 <akcelortiz@gmail.com>
2022-11-17 17:06:38 -0500

    El Login ha sido colocado dentro de una clase para poder acceder a funcionalidades de otras clases y ha sido reestructurado.

--- __main__.py
+++ __main__.py
@@ -1,56 +1,61 @@
+from Clase_Admin import *
+from Clase_Clientes import *
+from Funciones_DB import *
 import sqlite3
 import os
 clear = lambda: os.system('cls')
 
-db_nombre = "login.db"
+class Log():
+    db_nombre = "login.db"
 
-def Home():
-    clear()
-    print("HOME")
-    print("1- SIGNUP")
-    print("2- LOGIN")
-    while True:
-        print()
-        userChoice = input("Choose An Option: ")
-        if userChoice in ['1', '2']:
-            break
-    if userChoice == '1':
-        Signin()
-    else:
-        Login()
-
-def Signin():
-    clear()
-    print("Registrar usuario")
-
-def Login():
-    #coneccion con la base de datos
-    db = sqlite3.connect('login.db')
-    c = db.cursor()
-    print("     LOGIN BIBLIOTECA")
-    user = input("Ingrese usuario: ")
-    passw = input("Ingrese contraseña: ")
-
-    c.execute('SELECT * FROM users WHERE user = ? AND password = ?', (user, passw))
-
-    if c.fetchall():
+    def Home(self):
         clear()
-        c.execute('SELECT lvl FROM users WHERE user = ? AND password = ?', (user, passw))
-        lvl = c.fetchall()
-
-        print("Usuario correcto!")
-        print("Ingresando al sistema")
-        if lvl == [(1,)]:
-            print("Bienvenido Usuario")
-        elif lvl == [(3,)]:
-            print("Bienvenido Admin")
+        print("HOME")
+        print("1- LOGIN")
+        print("2- SALIR")
+        while True:
+            print()
+            userChoice = input("Choose An Option: ")
+            if userChoice in ['1', '2']:
+                break
+        if userChoice == '1':
+            self.Login()
         else:
-            print("Algo salió mal")
-            print(lvl)
+            exit
 
-    else:
-        print("Ups, Usuario y/o contraseña incorrectos")
+    def Login(self):
+        #coneccion con la base de datos
+        db = sqlite3.connect('login.db')
+        c = db.cursor()
+        print("     LOGIN BIBLIOTECA")
+        user = input("Ingrese usuario: ")
+        passw = input("Ingrese contraseña: ")
 
-    c.close()
+        c.execute('SELECT * FROM users WHERE user = ? AND password = ?', (user, passw))
 
-Home()
+        if c.fetchall():
+            clear()
+            c.execute('SELECT lvl FROM users WHERE user = ? AND password = ?', (user, passw))
+            lvl = c.fetchall()
+
+            print("Usuario correcto!")
+            print("Ingresando al sistema")
+            if lvl == [(1,)]:
+                print("Bienvenido Usuario")
+                app = Pedidos()
+                app.ventanaPrincipal()
+            elif lvl == [(3,)]:
+                print("Bienvenido Admin")
+                app2 = Admin()
+                app2.ventanaPrincipal()
+            else:
+                print("Algo salió mal")
+                print(lvl)
+
+        else:
+            print("Ups, Usuario y/o contraseña incorrectos")
+
+        c.close()
+
+app3 = Log()
+app3.Home()
