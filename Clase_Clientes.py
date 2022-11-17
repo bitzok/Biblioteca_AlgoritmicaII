b945719be5cc9d6eb26bba0a2f25e613ed1f33e4
bitzzz <santacruzedward@hotmail.com>
2022-11-17 14:50:38 -0500

    Actualizacion de la Clase Clientes, arreglando errores y conectando con FuncionesDB

--- Clase_Clientes.py
+++ Clase_Clientes.py
@@ -1,8 +1,4 @@
-from tkinter import ttk
-from tkinter import *
-from tkinter.messagebox import *
-import sqlite3 as sql
-
+from Funciones_DB import *
 from abc import ABC, abstractmethod
 
 class I_Clases(ABC):
@@ -14,33 +10,87 @@ class I_Clases(ABC):
     def consultar(self, consulta, parametros):
         pass
 
-class VentanaPrincipal():
-    def __init__(self):
-        self.raiz = Tk()
-        
-class Pedidos(VentanaPrincipal):
+    @abstractmethod
+    def VolverMenuPrincipal(self):
+        pass
+
+class Pedidos(I_Clases):
 
     db_nombre = "ListaClientes.db"
 
     def __init__(self):
-        super().__init__()
-        self.wind = self.raiz
-        self.wind.title('Nuevos pedidos')
-        self.wind.geometry("600x400+500+250")
-        self.wind.columnconfigure(0, weight = 1)
-        self.wind.rowconfigure(0, weight = 1)
+        self._nombres = None
+        self._apellido = None
+        self._dni = None
+        self._membresia = None
+
+
+    def agregarCliente(self):
+        self.nombre =  str(input("Ingrese el Nombre del nuevo cliente: "))
+        self.apellido =  str(input("Ingrese el Apellido del nuevo cliente: "))    
+        self.dni = str(input("Ingresar el DNI del nuevo cliente: "))
+        self.membresia = int(input("Ingresar el Nivel de mebresia del nuevo cliente: "))
+
+        if len(self.nombre) != 0 and len(self.apellido) != 0 and len(self.dni) != 0 and self.membresia > 0:
+            Funciones_Admin_Db.agregarClientes(self)
+        else:
+            print("Ingrese correctamente los campos solicitados.")
+        self.VolverMenuPrincipal()
+
+
+    def eliminarCliente(self):
+        self.dni =  str(input("Ingrese el DNI del Cliente a eliminar: "))
+
+        if len(self.dni) != 0:
+             Funciones_Admin_Db.borrarClientes(self)
+        else:
+            print("Ingrese correctamente el campo solicitado.")
+        self.VolverMenuPrincipal()
+        
+
+    def modificarNivel(self):
+        self.dni =  str(input("Ingrese el DNI del Cliente a modificar: "))
+        self.membresia = int(input("Ingresar el nuevo nivel de su mebresía: "))
+        if len(self.dni) != 0 and self.membresia > 0:
+            Funciones_Admin_Db.editarNivel(self)
+        else:
+            print("Ingrese correctamente los campos solicitados.")
+        self.VolverMenuPrincipal()
+
+
+    def mostrarCliente(self):
+        print("Se mostrará Los Clientes: ")
+        Funciones_Admin_Db.traerClientes(self)
+        self.VolverMenuPrincipal()
 
 
     def ventanaPrincipal(self):
-        Principal = LabelFrame(self.wind, text = "Ingrese la opcion deseada: ")
-        self.principal = Principal
-        Principal.grid(row = 0, column = 0, columnspan = 3, pady = 20)
-        ttk.Button(Principal, text = "Registrar Usuario", command = lambda : self.nuevoUsuario()).grid(row = 1, columnspan = 2, sticky = W + E)
-        ttk.Button(Principal, text = "Eliminar Usuario", command = lambda : self.eliminarUsuario()).grid(row = 2, columnspan = 2, sticky = W + E)
-        ttk.Button(Principal, text = "Mostrar Usuarios", command = lambda : self.mostrarUsuario()).grid(row = 3, columnspan = 2, sticky = W + E)
-    
-        self.raiz.mainloop()
-            
+        print("1. Agregar Cliente.")
+        print("2. Eliminar Cliente.")
+        print("3. Modificar Nivel del Cliente.")
+        print("4. Mostrar Clientes totales.")
+        opcion = int(input("Ingrese la opcion que desee:"))
+       
+        if opcion == 1: 
+            self.agregarCliente()
+        elif opcion == 2:
+            self.eliminarCliente()
+        elif opcion == 3:
+            self.modificarNivel()
+        elif opcion == 4: 
+            self.mostrarCliente()
+        else:
+            print("Ingrese una opcion valida.")
+
+
+    def VolverMenuPrincipal(self):
+        print("Desea volver al menu principal?")
+        opcion2 = int(input("1. Sí, 2. No."))
+        if opcion2 == 1:
+            self.ventanaPrincipal()
+        else:
+            print("Hasta luego.")
+
 
     def consultar(self, query, parameters = ()):
         with sql.connect(self.db_nombre) as conn:
@@ -48,93 +98,9 @@ class Pedidos(VentanaPrincipal):
             cursor.execute(query, parameters) 
             result = cursor.execute(query, parameters)
             conn.commit()
-        return result 
-
-
-    def nuevoUsuario(self):
-        cuadro = LabelFrame(self.wind, text = 'Ingrese el nuevo usuario: ')
-        cuadro.grid(row = 0, column = 0, columnspan = 3, pady = 20)
-        #IngresarNombre
-        Label(cuadro, text = 'Nombre: ').grid(row = 1, column = 0)
-        self.nombre = Entry(cuadro)
-        #self.nombre.focus()
-        self.nombre.grid (row = 1, column = 1)
-        #IngresarApellido
-        Label(cuadro, text = 'Apellido: ').grid(row = 2, column = 0)
-        self.apellido= Entry(cuadro)
-        self.apellido.grid (row = 2, column = 1)
-        #IngresarDNI
-        Label(cuadro, text = 'DNI: ').grid(row = 3, column = 0)
-        self.dni = Entry(cuadro)
-        self.dni.grid(row = 3, column = 1)
-        #IngresarLvlMembresía
-        Label(cuadro, text = 'Nivel Membresía: ').grid(row = 4, column = 0)
-        self.nivel = Entry(cuadro)
-        self.nivel.grid(row = 4, column = 1)
-        #BotónAgregarProducto
-        self.principal.destroy()
-        ttk.Button(cuadro, text = 'Agregar', command = self.agregarUsuario).grid(row = 5, columnspan = 2, sticky = W + E)
-        
-
-    def validarUsuario(self):
-        return len(self.nombre.get()) and len(self.apellido.get()) != 0 and len(self.dni.get()) != 0 and len(self.nivel.get()) != 0 
-
-
-    def eliminarUsuario(self):
-        cuadro2 = LabelFrame(self.wind, text = 'Ingrese el DNI del usuario a eliminar: ')
-        cuadro2.grid(row = 0, column = 0, columnspan = 3, pady = 40)
-        #IngresarCodigo
-        Label(cuadro2, text = 'DNI: ').grid(row = 1, column = 0)
-        self.dni = Entry(cuadro2)
-        self.dni.focus()
-        self.dni.grid (row = 1, column = 1)
-        self.principal.destroy()
-       #BotónBorrarProducto
-        ttk.Button(cuadro2, text = 'Borrar', command = self.borrarUsuario).grid(row = 5, columnspan = 4, sticky = W + E)
-
-
-    def borrarUsuario(self):
-        borrar = self.dni.get()
-        query = f"DELETE FROM DatosClientes WHERE DNI = ?"
-        self.consultar(query, [borrar])
-        showinfo(title = "¡Genial!", message = "Cliente borrado exitosamente")
-
-
-    def mostrarUsuario(self):
-        cuadro3 = LabelFrame(self.wind, text = 'Clientes registrados: ')
-        cuadro3.grid(row = 0, column = 0, columnspan = 3, pady = 40)
-        self.wind.geometry("800x600")
-        lista = ttk.Treeview(cuadro3, columns = (1, 2, 3), padding = "0", height = 8)
-
-        lista.heading("#0", text="Nombres", anchor=CENTER)
-        lista.heading("#1", text="Apellidos", anchor=CENTER)
-        lista.heading("#2", text="DNI", anchor=CENTER)
-        lista.heading("#3", text="Membresía", anchor=CENTER)
-
-        lista.grid(row = 0, column = 0, columnspan = 3, pady = 40)
-
-        elementos = self.retornoUsuario()
-
-        for i in elementos:
-            lista.insert("", 0, text=i[0], values=(i[1], i[2], i[3]))
-
-
-    def retornoUsuario(self):
-        pedir = "SELECT * FROM DatosClientes"
-        filas = self.consultar(pedir)
-        return filas
-
-
-    def agregarUsuario(self):
-        if self.validarUsuario():
-            query = 'INSERT or IGNORE INTO DatosClientes VALUES(?, ?, ?, ?)'
-            parametros = (self.nombre.get(), self.apellido.get(), self.dni.get(), self.nivel.get())
-            self.consultar(query, parametros)
-            showinfo(title = "¡Genial!", message = "Cliente añadido exitosamente")
-        else:
-            print("Los campos son requeridos.")
+        return result
 
 
 if __name__ == "__main__":
     app = Pedidos()
-    app1 = Pedidos.ventanaPrincipal(app)
+    app.ventanaPrincipal()
--- Funciones_DB.py
+++ Funciones_DB.py
@@ -7,22 +7,48 @@ class Funciones_Admin_Db():
         db_filas = self.consultar(query)
         for fila in db_filas:
             print(fila)
+
+    def traerClientes(self):
+        query = 'SELECT * FROM DatosClientes'
+        db_filas = self.consultar(query)
+        for fila in db_filas:
+            print(fila)
     
+
     def agregarProductos(self):
         query = 'INSERT or IGNORE INTO Libros VALUES(?, ?, ?, ?)'
         parametros = (self.codigo, self.nombre, self.autor, self.stock)
         self.consultar(query, parametros)
         print("Los datos han sido guardados.")
 
+    def agregarClientes(self):
+        query = 'INSERT or IGNORE INTO DatosClientes VALUES(?, ?, ?, ?)'
+        parametros = (self.nombre, self.apellido, self.dni, self.membresia)
+        self.consultar(query, parametros)
+        print("Los datos han sido guardados.")    
+
+
     def borrarProductos(self):
         borrar = self.codigo
         query = "DELETE FROM Libros WHERE Codigo = ?"
         self.consultar(query, [borrar])
         print("Los datos han sido borrados.")
+
+    def borrarClientes(self):
+        borrar = self.dni
+        query = "DELETE FROM DatosClientes WHERE DNI = ?"
+        self.consultar(query, [borrar])
+        print("Los datos han sido borrados.")
     
+
     def editarStock(self):
         parametros = (self.stock, self.codigo)
         query = "UPDATE Libros SET Stock = ? WHERE Codigo = ?"
         self.consultar(query, parametros)
         print("El stock ha sido modificado correctamente.")
-    
+
+    def editarNivel(self):
+        parametros = (self.membresia, self.dni)
+        query = "UPDATE DatosClientes SET Membresia = ? WHERE DNI = ?"
+        self.consultar(query, parametros)
+        print("La membresia ha sido modificado correctamente.")
