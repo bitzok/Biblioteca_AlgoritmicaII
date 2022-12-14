88ac951ac9efe12298e05f0e4f18832c39cce748
Dwar325 <akcelortiz@gmail.com>
2022-12-13 21:48:02 -0500

    Agregadas nuevas funcionalidades

--- Funciones_DB.py
+++ Funciones_DB.py
@@ -14,7 +14,6 @@ class Funciones_Admin_Db():
         for fila in db_filas:
             print(fila)
     
-
     def agregarProductos(self):
         query = 'INSERT or IGNORE INTO Libros VALUES(?, ?, ?, ?)'
         parametros = (self.codigo, self.nombre, self.autor, self.stock)
@@ -40,7 +39,6 @@ class Funciones_Admin_Db():
         self.consultar(query, [borrar])
         print("Los datos han sido borrados.")
     
-
     def editarStock(self):
         parametros = (self.stock, self.codigo)
         query = "UPDATE Libros SET Stock = ? WHERE Codigo = ?"
@@ -52,3 +50,37 @@ class Funciones_Admin_Db():
         query = "UPDATE DatosClientes SET Membresia = ? WHERE DNI = ?"
         self.consultar(query, parametros)
         print("La membresia ha sido modificado correctamente.")
+
+    def BuscarLibro(self):
+        query = "SELECT * from Libros WHERE Nombre LIKE '"+str(self.nombres)+"%'" 
+        Db_filas = self.consultar(query)
+        print("Se han encontrado los siguientes resultados: ")
+        for fila in Db_filas:
+            print(fila)
+
+    def PedirLibro(self):
+        query = "SELECT Stock from Libros WHERE Codigo = ?"
+        stock = self.consultar(query, self.codigo)
+        for stocks in stock:
+            stock_nuevo = ''.join(str(i) for i in stocks)
+        if int(stock_nuevo) >= 1:
+            query_2 = "UPDATE Libros SET Stock = Stock - 0.5 WHERE Stock > 0 and Codigo = ?"
+            self.consultar(query_2, self.codigo)
+            print("El libro ha sido entregado, recuerde entregarlo a tiempo.")
+        else:
+            print("El libro no se encuentra disponible, considere hacer una reserva.")
+            
+
+    def ReservarLibro(self):
+        query = "SELECT Stock from Libros WHERE Codigo = ?"
+        stock = self.consultar(query, self.codigo)
+        for stocks in stock:
+            stock_nuevo = ''.join(str(i) for i in stocks)
+        if int(stock_nuevo) == 0:
+            query = "SELECT Nombre from Libros WHERE Codigo = ?"
+            nombre = self.consultar(query, self.codigo)
+            for nombres in nombre:
+                Reserva = ''.join(str(i) for i in nombres)
+            print(f"Ha reservado el siguiente libro: {Reserva}")
+        else:
+            print("El libro está disponible para ser pedido.")
